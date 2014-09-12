0f9feb7a8a65c473bb022c7a8375a0f67cb69e06
Stephanie Yu <styu314@gmail.com>
2014-09-12 18:12:04 -0400

    Initial app. It's blank.

---
+++ Pong/Pong/PongAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PongAppDelegate.h
+//  Pong
+//
+//  Created by Stephanie Yu on 9/12/14.
+//  Copyright (c) 2014 Stephanie Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PongAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pong/Pong/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pong
+//
+//  Created by Stephanie Yu on 9/12/14.
+//  Copyright (c) 2014 Stephanie Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PongAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PongAppDelegate class]));
+    }
+}
